627485cd514292caff1025c2252a15df74d2c0b1
hwoss123 <306031875@qq.com>
2024-05-07 23:33:51 +0800

    增加日志框架，xml文件还没读取

--- web/src/main/java/com/web/Test.java
+++ web/src/main/java/com/web/Test.java
@@ -1,15 +1,17 @@
 package com.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class Test {
     @RequestMapping("/test")
     String test(){
         aa.builder().name("test");
-//        log.info("");
+        log.info("1111");
         return "nihao ";
     }
 
