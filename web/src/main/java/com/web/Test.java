9b829b90e4d7c975c511a1db74949b026fb90926
hwoss123 <306031875@qq.com>
2024-05-08 17:12:19 +0800

    修改日志不输出，springProfile全责

--- web/src/main/java/com/web/Test.java
+++ web/src/main/java/com/web/Test.java
@@ -1,17 +1,25 @@
 package com.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 @Slf4j
 public class Test {
+    private static final Logger LOG = Logger.getLogger(Test.class.getName());
+
     @RequestMapping("/test")
-    String test(){
-        aa.builder().name("test");
-        log.info("1111");
+    String test() {
+//        int a = 1/0;
+//        aa.builder().name("test");
+        log.info("Testing");
+        log.error("1111");
+//        LOG.("1111");
+        LOG.info("1111");
+        LOG.warning("1111");
         return "nihao ";
     }
 
