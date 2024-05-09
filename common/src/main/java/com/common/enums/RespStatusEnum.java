a4edcfc2df2cc12769d8a1ec125e07ad939efba6
hwoss123 <306031875@qq.com>
2024-05-09 23:40:17 +0800

    创建BasicResultVo作为返回类型共业务层调用，并且数据从RespStatusEnum枚举里面获得 这里优雅的写法就是链式调用多用this关键字进行当前对象的调用，以及以后这种含数据的最终返回类型用泛型解决不要去用Object

---
+++ common/src/main/java/com/common/enums/RespStatusEnum.java
@@ -0,0 +1,65 @@
+package com.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * @author Hwoss
+ * @date 2024/05/09
+ * 返回信息的状态
+ */
+@Getter
+@ToString
+@AllArgsConstructor
+public enum RespStatusEnum {
+    //枚举放在前面
+
+    /**
+     * 错误的编码
+     */
+    ERROR_500("500", "服务器发生错误"),
+    ERROR_400("400", "请求发生错误"),
+
+
+    /**
+     * TODO 这里进行了更改他的状态响应码，如果后续数据库配对不上可以返回
+     */
+    SUCCESS("1", "调用成功"),
+    FAIL("0", "调用失败"),
+
+    /**
+     * 客户端
+     */
+    CLIENT_BAD_PARAMETER("C01", "客户端的参数配置错误"),
+    TEMPLATE_NOT_FOUND("C02", "模板找不到对应的数据"),
+    TOO_MANY_REQUEST("C03", "请求大于100"),
+    NO_LOGIN("C04", "请先登录"),
+    LOGIN("C05", "测试环境，无需登录"),
+
+    /**
+     * 服务端
+     */
+    SERVICE_ERROR("S01", "服务器出现异常"),
+    RESOURCE_NOT_FOUND("S02", "服务资源找不到"),
+
+    /**
+     * 对于对于责任链通道的校验需要
+     */
+    CONTEXT_IS_NULL("P01", "流程上下文为空"),
+    BUSINESS_CODE_IS_NULL("P02", "业务代码为空"),
+    PROCESS_TEMPLATE_IS_NULL("P03", "流程模板为空"),
+    PROCESS_LIST_IS_NULL("P04", "业务流程处理器为空"),
+
+    ;
+    /**
+     * 返回状态
+     */
+    private final String code;
+    /**
+     * 返回的信息
+     */
+    private final String msg;
+
+
+}
---
+++ common/src/main/java/com/common/vo/BasicResultVo.java
@@ -0,0 +1,126 @@
+package com.common.vo;
+
+
+import com.common.enums.RespStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author Hwoss
+ * @date 2024/05/09
+ * 作为最终返回的结果RespResultEnum的CODE和MSG
+ */
+@Getter
+@ToString(callSuper = true)//自动toString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public final class BasicResultVo<T> {
+    //返回的结果状态也就是
+    private String status;
+
+    //返回的消息编码
+    private String msg;
+
+    private T data;
+
+    public BasicResultVo(String status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public BasicResultVo(String status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+
+    public BasicResultVo(RespStatusEnum status, T data) {
+        this.status = status.getCode();
+        this.msg = status.getMsg();
+        this.data = null;
+    }
+
+    /**
+     * @param status
+     * @param msg
+     * @param data   这个方法可以作为直接从外部传入枚举值
+     */
+    public BasicResultVo(RespStatusEnum status, String msg, T data) {
+        this.status = status.getCode();
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public BasicResultVo(RespStatusEnum status) {
+        this(status, null);
+    }
+
+    /**
+     * @return {@link BasicResultVo }<{@link T }>
+     * 默认成功调用
+     */
+    public static <T> BasicResultVo<T> success() {
+        return new BasicResultVo<>(RespStatusEnum.SUCCESS,
+                                   RespStatusEnum.SUCCESS.getMsg(), null);
+    }
+
+    /**
+     * @param msg
+     * @return {@link BasicResultVo }<{@link T }>
+     * 用于对自定义成功信息的调用
+     */
+    public static <T> BasicResultVo<T> success(String msg) {
+        return new BasicResultVo<T>(RespStatusEnum.SUCCESS, msg, null);
+    }
+
+    /**
+     * @param data
+     * @return {@link BasicResultVo }<{@link T }>
+     * 用于对自定义成功数据的调用
+     */
+    public static <T> BasicResultVo<T> success(T data) {
+        return new BasicResultVo<T>(RespStatusEnum.SUCCESS, data);
+    }
+
+    /**
+     * @return {@link BasicResultVo }<{@link T }>
+     * 默认失败调用
+     */
+    public static <T> BasicResultVo<T> fail() {
+        return new BasicResultVo<T>(RespStatusEnum.FAIL,
+                                    RespStatusEnum.FAIL.getMsg(), null);
+    }
+
+    /**
+     * @param msg
+     * @return {@link BasicResultVo }<{@link T }>
+     * 自定义错误信息
+     */
+    public static <T> BasicResultVo<T> fail(String msg) {
+        return fail(RespStatusEnum.FAIL, msg);
+    }
+
+    /**
+     * 自定义状态和信息的失败响应
+     *
+     * @param status 状态
+     * @param msg    信息
+     * @return 自定义状态和信息的失败响应
+     */
+    public static <T> BasicResultVo<T> fail(RespStatusEnum status, String msg) {
+        return new BasicResultVo<>(status, msg, null);
+    }
+
+    /**
+     * 自定义状态的失败响应
+     *
+     * @param status 状态
+     * @return 自定义状态的失败响应
+     */
+    public static <T> BasicResultVo<T> fail(RespStatusEnum status) {
+        return fail(status, status.getMsg());
+    }
+}
