7db7573f8b0ee41c1702c2e4a0d1dce8d6fcc2eb
hwoss123 <306031875@qq.com>
2024-05-22 17:12:26 +0800

    对于获取所有改渠道的账号进行遍历找到需要的脚本，编写腾讯云的短信脚本，以及拉取回执信息

--- handler/src/main/java/com/hwoss/handler/script/impl/TencentSmsScript.java
+++ handler/src/main/java/com/hwoss/handler/script/impl/TencentSmsScript.java
@@ -1,20 +1,195 @@
 package com.hwoss.handler.script.impl;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.IdUtil;
+import com.alibaba.fastjson.JSON;
+import com.common.dto.account.TencentSmsAccount;
+import com.common.enums.SmsStatus;
+import com.google.common.base.Throwables;
 import com.hwoss.handler.domain.SmsParam;
 import com.hwoss.handler.script.SmsScript;
 import com.hwoss.suport.domain.SmsRecord;
+import com.hwoss.suport.utils.AccountUtils;
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+import com.tencentcloudapi.sms.v20210111.SmsClient;
+import com.tencentcloudapi.sms.v20210111.models.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
+@Slf4j
+@Component("TencentSmsScript")
 public class TencentSmsScript implements SmsScript {
+
+    private static final Integer PHONE_NUM = 11;
+
+
+    @Autowired
+    private AccountUtils accountUtils;
 
     @Override
     public List<SmsRecord> send(SmsParam smsParam) {
-        return null;
+        TencentSmsAccount tencentSmsAccount = Objects.nonNull(smsParam.getSendAccountId()) ?
+                accountUtils.getAccountById(smsParam.getSendAccountId(), TencentSmsAccount.class)
+                : accountUtils.getSmsAccountByScriptName(smsParam.getScriptName(), TencentSmsAccount.class);
+        try {
+            SmsClient smsClient = init(tencentSmsAccount);
+            SendSmsRequest sendSmsRequest = assembleSendReq(smsParam, tencentSmsAccount);
+            SendSmsResponse sendSmsResponse = smsClient.SendSms(sendSmsRequest);
+            return assembleSendSmsRecord(tencentSmsAccount, sendSmsResponse, smsParam);
+        } catch (TencentCloudSDKException e) {
+            log.error("TencentSmsScript#send fail:{},params:{}", Throwables.getStackTraceAsString(e), JSON.toJSONString(smsParam));
+            return new ArrayList<>();
+        }
     }
 
+
+    /**
+     * 初始化 client
+     *
+     * @param account
+     */
+    private SmsClient init(TencentSmsAccount account) {
+        Credential cred = new Credential(account.getSecretId(), account.getSecretKey());
+        HttpProfile httpProfile = new HttpProfile();
+        httpProfile.setEndpoint(account.getUrl());
+        ClientProfile clientProfile = new ClientProfile();
+        clientProfile.setHttpProfile(httpProfile);
+        return new SmsClient(cred, account.getRegion(), clientProfile);
+    }
+
+    /**
+     * 组装发送短信参数
+     *
+     * @param smsParam
+     * @param account
+     * @return {@link SendSmsRequest }
+     * 其实就是对数据获取到发送的数据和脚本数据的拼接
+     */
+    private SendSmsRequest assembleSendReq(SmsParam smsParam, TencentSmsAccount account) {
+        SendSmsRequest req = new SendSmsRequest();
+        String[] phoneNumberSet1 = smsParam.getPhones().toArray(new String[smsParam.getPhones().size() - 1]);
+        req.setPhoneNumberSet(phoneNumberSet1);
+        req.setSmsSdkAppId(account.getSmsSdkAppId());
+        req.setSignName(account.getSignName());
+        req.setTemplateId(account.getTemplateId());
+        String[] templateParamSet1 = {smsParam.getContent()};
+        req.setTemplateParamSet(templateParamSet1);
+        req.setSessionContext(IdUtil.fastSimpleUUID());
+        return req;
+    }
+
+
+    /**
+     * @param account
+     * @param response
+     * @param smsParam
+     * @return {@link List }<{@link SmsRecord }>
+     * 把返回的数据和对应的账号信息封装到Record里面
+     */
+    private List<SmsRecord> assembleSendSmsRecord(TencentSmsAccount account, SendSmsResponse response, SmsParam smsParam) {
+        List<SmsRecord> smsRecordList = new ArrayList<>();
+        if (Objects.isNull(response) || ArrayUtil.isEmpty(response.getSendStatusSet())) {
+            return smsRecordList;
+        }
+        SendStatus[] sendStatusSet = response.getSendStatusSet();
+        for (SendStatus sendStatus : sendStatusSet) {
+//        +8613711112222，翻转去前面的11位在翻转回去
+            String phone = new StringBuilder
+                    (new StringBuilder(sendStatus.getPhoneNumber()).reverse().substring(0, PHONE_NUM))
+                    .reverse().toString();
+
+            SmsRecord smsRecord = SmsRecord.builder()
+                    .chargingNum(Integer.parseInt(String.valueOf(sendStatus.getFee())))
+                    .created(Math.toIntExact(DateUtil.currentSeconds()))  //如果转换超出int范围就会直接报错
+                    .messageTemplateId(smsParam.getMessageTemplateId())
+                    .msgContent(smsParam.getContent())
+                    .phone(Long.valueOf(phone))
+                    .sendDate(Integer.valueOf(DateUtil.format(new Date(), DatePattern.PURE_DATE_PATTERN)))
+                    .supplierId(account.getSupplierId())
+                    .seriesId(sendStatus.getSerialNo())
+                    .updated(Math.toIntExact(DateUtil.currentSeconds()))
+                    .reportContent(sendStatus.getCode())
+                    .supplierName(account.getSupplierName())
+                    .status(SmsStatus.SEND_SUCCESS.getCode())//此时发送成功，但是还没收到回执
+                    .build();
+            smsRecordList.add(smsRecord);
+        }
+        return smsRecordList;
+    }
+
+    /**
+     * @param accountId
+     * @return {@link List }<{@link SmsRecord }>
+     * 拉取回执，类似渠道方的一个ack作用
+     */
     @Override
-    public List<SmsRecord> pull(Integer id) {
-        return null;
+    public List<SmsRecord> pull(Integer accountId) {
+        try {
+            TencentSmsAccount account = accountUtils.getAccountById(accountId, TencentSmsAccount.class);
+            SmsClient client = init(account);
+            PullSmsSendStatusRequest req = assemblePullReq(account);
+            PullSmsSendStatusResponse resp = client.PullSmsSendStatus(req);
+            return assemblePullSmsRecord(account, resp);
+        } catch (Exception e) {
+            log.error("TencentSmsReceipt#pull fail!{}", Throwables.getStackTraceAsString(e));
+            return new ArrayList<>();
+        }
+    }
+
+    /**
+     * 组装 拉取回执 入参
+     *
+     * @param account
+     * @return
+     */
+    private PullSmsSendStatusRequest assemblePullReq(TencentSmsAccount account) {
+        PullSmsSendStatusRequest req = new PullSmsSendStatusRequest();
+//        设置了拉取短信发送状态的数量限制为 10 条。
+        req.setLimit(10L);
+        req.setSmsSdkAppId(account.getSmsSdkAppId());
+        return req;
+    }
+
+    /**
+     * 组装 拉取回执信息
+     *
+     * @param account
+     * @param resp
+     * @return
+     */
+    private List<SmsRecord> assemblePullSmsRecord(TencentSmsAccount account, PullSmsSendStatusResponse resp) {
+        List<SmsRecord> smsRecordList = new ArrayList<>();
+        if (Objects.nonNull(resp) && Objects.nonNull(resp.getPullSmsSendStatusSet()) && resp.getPullSmsSendStatusSet().length > 0) {
+            for (PullSmsSendStatus pullSmsSendStatus : resp.getPullSmsSendStatusSet()) {
+                SmsRecord smsRecord = SmsRecord.builder()
+                        .sendDate(Integer.valueOf(DateUtil.format(new Date(), DatePattern.PURE_DATE_PATTERN)))
+                        .messageTemplateId(0L)
+                        .phone(Long.valueOf(pullSmsSendStatus.getSubscriberNumber()))
+                        .supplierId(account.getSupplierId())
+                        .supplierName(account.getSupplierName())
+                        .msgContent("")
+                        .seriesId(pullSmsSendStatus.getSerialNo())
+                        .chargingNum(0)
+//                        主要看响应码是否进行一个下发成功
+                        .status("SUCCESS".equals(pullSmsSendStatus.getReportStatus()) ? SmsStatus.RECEIVE_SUCCESS.getCode() : SmsStatus.RECEIVE_FAIL.getCode())
+                        .reportContent(pullSmsSendStatus.getDescription())
+                        .updated(Math.toIntExact(pullSmsSendStatus.getUserReceiveTime()))
+                        .created(Math.toIntExact(DateUtil.currentSeconds()))
+                        .build();
+                smsRecordList.add(smsRecord);
+            }
+        }
+        return smsRecordList;
     }
 }
--- support/src/main/java/com/hwoss/suport/utils/AccountUtils.java
+++ support/src/main/java/com/hwoss/suport/utils/AccountUtils.java
@@ -1,6 +1,9 @@
 package com.hwoss.suport.utils;
 
 import com.alibaba.fastjson.JSON;
+import com.common.constant.CommonConstant;
+import com.common.dto.account.SmsAccount;
+import com.common.enums.ChannelType;
 import com.google.common.base.Throwables;
 import com.hwoss.suport.dao.ChannelAccountDao;
 import com.hwoss.suport.domain.ChannelAccount;
@@ -8,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
 import java.util.Optional;
 
 
@@ -19,6 +24,12 @@ public class AccountUtils {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
+    /**
+     * @param sendAccountId
+     * @param clazz
+     * @return {@link T }
+     * 根据id获取对应渠道账号的脚本数据写进去
+     */
     public <T> T getAccountById(Integer sendAccountId, Class<T> clazz) {
         try {
             Optional<ChannelAccount> optionalChannelAccount = channelAccountDao.findById(Long.valueOf(sendAccountId));
@@ -32,4 +43,32 @@ public class AccountUtils {
         return null;
     }
 
+    /**
+     * @param scriptName
+     * @param tClass
+     * @return {@link T }
+     * 获取所有改渠道的账号进行遍历找到需要的脚本
+     */
+    public <T> T getSmsAccountByScriptName(String scriptName, Class<T> tClass) {
+        try {
+            List<ChannelAccount> channelAccountList = channelAccountDao.findAllByIsDeletedEqualsAndSendChannelEquals(CommonConstant.FALSE, ChannelType.SMS.getCode());
+            for (ChannelAccount channelAccount : channelAccountList) {
+
+                try {
+                    SmsAccount smsAccount = JSON.parseObject(channelAccount.getAccountConfig(), SmsAccount.class);
+                    //      此时里面还有渠道商id和渠道商名字
+                    if (smsAccount.getScriptName().equals(scriptName)) {
+                        //            返回的是 TencentSmsAccount具体的脚本对象
+                        return JSON.parseObject(smsAccount.getScriptName(), tClass);
+                    }
+                } catch (Exception e) {
+                    log.error("AccountUtils#getSmsAccount fail! e:{}", Throwables.getStackTraceAsString(e));
+                }
+            }
+        } catch (Exception e) {
+            log.error("AccountUtils#getSmsAccount fail! e:{}", Throwables.getStackTraceAsString(e));
+        }
+        log.error("AccountUtils#getSmsAccount not found!:{}", scriptName);
+        return null;
+    }
 }
