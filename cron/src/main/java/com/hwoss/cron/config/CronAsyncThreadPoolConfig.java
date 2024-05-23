945f2ff7f6a4046cfcfacfde04ab6ce35df540b8
hwoss123 <306031875@qq.com>
2024-05-24 00:11:48 +0800

    定时任务处理的线程池

---
+++ cron/src/main/java/com/hwoss/cron/config/CronAsyncThreadPoolConfig.java
@@ -0,0 +1,64 @@
+package com.hwoss.cron.config;
+
+import cn.hutool.core.thread.ExecutorBuilder;
+import com.common.constant.CommonConstant;
+import com.common.constant.ThreadPoolConstant;
+import com.dtp.common.em.QueueTypeEnum;
+import com.dtp.common.em.RejectedTypeEnum;
+import com.dtp.core.thread.DtpExecutor;
+import com.dtp.core.thread.ThreadPoolBuilder;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Hwoss
+ * @date 2024/05/24
+ * 用于定时任务的动态线程池，以配置中心的配置为先
+ */
+public class CronAsyncThreadPoolConfig {
+
+    public static final String EXECUTE_XXL_THREAD_POOL_NAME = "execute-xxl-thread-pool";
+
+    private CronAsyncThreadPoolConfig() {
+    }
+
+    /**
+     * 业务：消费pending队列实际的线程池
+     * 配置：核心线程可以被回收，当线程池无被引用且无核心线程数，应当被回收
+     *
+     * @return
+     */
+    public static ExecutorService getConsumePendingThreadPool() {
+        return ExecutorBuilder.create()
+                .setHandler(new ThreadPoolExecutor.CallerRunsPolicy())
+                .setMaxPoolSize(ThreadPoolConstant.COMMON_MAX_POOL_SIZE)
+                .setCorePoolSize(ThreadPoolConstant.COMMON_CORE_POOL_SIZE)
+                .setAllowCoreThreadTimeOut(true)
+                .setKeepAliveTime(ThreadPoolConstant.SMALL_KEEP_LIVE_TIME, TimeUnit.SECONDS)
+                .setWorkQueue(new LinkedBlockingQueue<>(ThreadPoolConstant.BIG_QUEUE_SIZE))
+                .build();
+    }
+
+    /**
+     * 业务：接收到xxl-job请求的线程池
+     * 配置：不丢弃消息，核心线程数不会随着keepAliveTime而减少(不会被回收)
+     * 动态线程池且被Spring管理
+     *
+     * @return
+     */
+    public static DtpExecutor getXxlCronExecutor() {
+        return ThreadPoolBuilder.newBuilder()
+                .threadPoolName(EXECUTE_XXL_THREAD_POOL_NAME)
+                .corePoolSize(ThreadPoolConstant.COMMON_CORE_POOL_SIZE)
+                .maximumPoolSize(ThreadPoolConstant.COMMON_MAX_POOL_SIZE)
+                .keepAliveTime(ThreadPoolConstant.COMMON_KEEP_LIVE_TIME)
+                .timeUnit(TimeUnit.SECONDS)
+                .rejectedExecutionHandler(RejectedTypeEnum.CALLER_RUNS_POLICY.getName())
+                .allowCoreThreadTimeOut(false)
+                .workQueue(QueueTypeEnum.VARIABLE_LINKED_BLOCKING_QUEUE.getName(), ThreadPoolConstant.COMMON_QUEUE_SIZE, false)
+                .buildDynamic();
+    }
+}
---
+++ cron/src/main/java/com/hwoss/cron/handler/CronTaskHandler.java
@@ -0,0 +1,8 @@
+package com.hwoss.cron.handler;
+
+/**
+ * @author Hwoss
+ * @date 2024/05/23
+ */
+public class CronTaskHandler {
+}
